b64f0ee94771ea5134161503c3cb088abb6b91c2
Robert Kein <robertkejn@gmail.com>
2022-11-30 12:25:38 +0400

    Initial commit

---
+++ ht_7.1/ht_7.1.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+#define MODE 1
+
+#if !defined(MODE)
+#error Ошибка, констатна не определена
+#elif MODE == 1
+int add(int a, int b) {
+	return a + b;
+}
+#define STATE	int a = 0; \
+				int b = 0; \
+				std::cout << "Работаю в боевом режиме\n";\
+				std::cout << "Введите число 1: ";\
+				std::cin >> a; \
+				std::cout << "Введите число 2: ";\
+				std::cin >> b;\
+				std::cout << "Результат сложения: " << add(a, b);
+
+#elif MODE == 0
+
+#define STATE std::cout<<"Работаю в тестовом режиме";
+
+#else
+
+#define STATE std::cout<<"Неизвестный режим. Завершение работы";
+
+#endif
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	STATE;
+}
